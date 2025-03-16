2bf29a30ec8711610a016e9f0fed1bb6ce0ae73a
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-16 07:29:53 -0400

    Created a jobDispatcher abstract class

---
+++ src/JobDispatcher.java
@@ -0,0 +1,10 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of the class: An abstract class that functions as a blue-print for different types of job dispatchers.
+ * Different job dispatchers can implement their own logic for how jobs are dispatched to servers.
+ */
+
+public abstract class JobDispatcher {
+    
+}
