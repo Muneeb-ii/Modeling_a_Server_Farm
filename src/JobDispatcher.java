fe613bbe09ce1b923e5a914f626a53083a6e4c1a
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-16 07:35:23 -0400

    Added required fields

--- src/JobDispatcher.java
+++ src/JobDispatcher.java
@@ -5,6 +5,12 @@
  * Different job dispatchers can implement their own logic for how jobs are dispatched to servers.
  */
 
+import java.util.ArrayList;
+
 public abstract class JobDispatcher {
-    
+
+    private ArrayList<Server> servers; // list of servers
+    private double sysTime; // system time
+    private LinkedList<Job> jobQueue; // queue of jobs waiting to be dispatched
+    private ServerFarmViz serverFarmViz; // visualization object
 }
