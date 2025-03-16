334a3fa96bb25d1fa889a8f9834b04542cf158c5
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-16 07:42:55 -0400

    Added two new fields and a constructor

--- src/JobDispatcher.java
+++ src/JobDispatcher.java
@@ -11,6 +11,23 @@ public abstract class JobDispatcher {
 
     private ArrayList<Server> servers; // list of servers
     private double sysTime; // system time
-    private LinkedList<Job> jobQueue; // queue of jobs waiting to be dispatched
+    private LinkedList<Job> jobsHandled;// list of jobs handled
+    private int numOfJobsHandled; // number of jobs handled 
     private ServerFarmViz serverFarmViz; // visualization object
+
+    /**
+     * Constructor for the JobDispatcher class
+     * @param k the number of servers
+     * @param showViz whether to show visualization or not
+     */
+    public JobDispatcher(int k, boolean showViz){
+        servers = new ArrayList<>();
+        for(int i=0; i<k; i++){ // create k servers
+            servers.add(new Server());
+        }
+        sysTime = 0.0;
+        jobsHandled = new LinkedList<Job>();
+        numOfJobsHandled = 0;
+        serverFarmViz = new ServerFarmViz(this, showViz);
+    }
 }
