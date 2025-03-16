b40f60cff031d5feeef4f3e850f0156cc25e9b8f
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-16 05:53:47 -0400

    Added a constructor

--- src/Server.java
+++ src/Server.java
@@ -10,4 +10,15 @@ public class Server {
     LinkedList<Job> jobQueue; // queue of jobs waiting to be processed
     double sysTime; // system time
     double remainingTime; // remaining time for the current job being processed
+    int numOfJobsProcessed; // number of jobs processed
+    
+    /**
+     * Constructor for the Server class
+     */
+    public Server(){
+        jobQueue = new LinkedList<Job>();
+        sysTime = 0.0;
+        remainingTime = 0.0;
+        numOfJobsProcessed = 0;
+    }
 }
