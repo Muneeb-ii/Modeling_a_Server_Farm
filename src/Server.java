304670379d177b3b15a7669dbdf68db8d2f2fd27
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-16 05:50:32 -0400

    Added a Server class

---
+++ src/Server.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of the class: 
+ * 
+ */
+
+public class Server {
+    
+    LinkedList<Job> jobQueue; // queue of jobs waiting to be processed
+    double sysTime; // system time
+    double remainingTime; // remaining time for the current job being processed
+}
